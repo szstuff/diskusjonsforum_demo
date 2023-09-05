3f82262cad3a48732567645aea0dbb266bde29f3
szstuff <stilianzagorov1@gmail.com>
2023-09-05 10:47:57 +0200

    Initial commit

---
+++ diskusjonsforum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace diskusjonsforum.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
