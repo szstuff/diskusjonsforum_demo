32e6462d86c99915e1f970873b0deb649be3817f
szstuff <stilianzagorov1@gmail.com>
2023-09-05 11:45:52 +0200

    Created models for Comment, User and Post

---
+++ diskusjonsforum/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace diskusjonsforum.Models
+{
+	public class Comment
+	{
+		public int CommentID { get; set; }
+		public String Body { get; set; }
+		public int parentID { get; set; } //Parent ID, either post or comment
+		public int UserID { get; set; } //FK: Creator UserID 
+	}
+}
+
---
+++ diskusjonsforum/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace diskusjonsforum.Models
+{
+	public class Post
+	{
+		public int PostID { get; set; }
+        public String Title { get; set; }
+        //public String Category { get; set; } //Like a subreddit
+        public String Body { get; set; }
+        public int UserID { get; set; } // Foreign key: creator UserID
+        
+
+    }
+}
+
