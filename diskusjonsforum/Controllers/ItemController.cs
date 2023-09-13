883a7ff805082849336597a558de8409b66bcadf
Saloni Arora Chitkara <s364538@oslomet.no>
2023-09-13 12:10:20 +0200

    ItemController lagd

---
+++ diskusjonsforum/Controllers/ItemController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using diskusjonsforum_ITPE3200
+
+	namespace diskusjonsforum_ITPE3200
+public class ItemController : Controller
+{
+	public IActionResult ()
+	{
+	}
+}
--- diskusjonsforum/Models/User.cs
+++ diskusjonsforum/Models/User.cs
@@ -5,7 +5,7 @@ namespace diskusjonsforum.Models
 	public class User
 	{
 		public int UserID { get; set; }
-        public String Name { get; set; }
+        public  String Name { get; set; }
         public String Email { get; set; }
         public String PasswordHash { get; set; }
         public bool Administrator { get; set; } = false;
