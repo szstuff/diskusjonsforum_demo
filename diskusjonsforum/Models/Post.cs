116008258994eba051ad92583435f7b4e1a14121
szstuff <stilianzagorov1@gmail.com>
2023-09-13 11:56:43 +0200

    set up some of the database code
    not fully functional

--- diskusjonsforum/Models/Post.cs
+++ diskusjonsforum/Models/Post.cs
@@ -1,15 +1,14 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 namespace diskusjonsforum.Models
 {
-	public class Post
+    public class Post
 	{
 		public int PostID { get; set; }
-        public String Title { get; set; }
+        public required String Title { get; set; }
         //public String Category { get; set; } //Like a subreddit
-        public String Body { get; set; }
+        public String? Body { get; set; }
         public int UserID { get; set; } // Foreign key: creator UserID
-        
-
     }
 }
 
--- diskusjonsforum/Program.cs
+++ diskusjonsforum/Program.cs
@@ -1,7 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using diskusjonsforum.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ItemDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+});
+
 
 var app = builder.Build();
 
