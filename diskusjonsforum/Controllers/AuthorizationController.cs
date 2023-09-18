c31598879b633288296c360b7d4326ed64d29f48
szstuff <stilianzagorov1@gmail.com>
2023-09-18 12:43:53 +0200

    Corrected issues with Authcontroller (wrong naming convention) "Login" button on website now redirects to login page without crashing

---
+++ diskusjonsforum/Controllers/AuthorizationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using diskusjonsforum.Models;
+
+namespace diskusjonsforum.Controllers;
+
+public class AuthorizationController : Controller
+{
+    private readonly ILogger<AuthorizationController> _logger;
+
+    public AuthorizationController(ILogger<AuthorizationController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
