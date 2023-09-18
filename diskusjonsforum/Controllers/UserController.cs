f4758478522fe1bd3973e47640b0689825729d35
Jenny Hovland <jenny.hovland@hva.nl>
2023-09-18 11:36:12 +0200

    Fixed small erros of "class1"

--- diskusjonsforum/Controllers/UserController.cs
+++ diskusjonsforum/Controllers/UserController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-public class Class1
+public class Class3
 {
-	public Class1()
+	public Class3()
 	{
 	}
 }
--- diskusjonsforum/Models/Comment.cs
+++ diskusjonsforum/Models/Comment.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-public class Class1
+public class Class4
 {
-	public Class1()
+	public Class4()
 	{
 	}
 }
