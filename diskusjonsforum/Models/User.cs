648d4b568900f4367d3e3f8d0b43a5e9a2056260
szstuff <stilianzagorov1@gmail.com>
2023-09-18 12:43:53 +0200

    Corrected issues with Authcontroller (wrong naming convention) "Login" button on website now redirects to login page without crashing

--- diskusjonsforum/Models/User.cs
+++ diskusjonsforum/Models/User.cs
@@ -4,7 +4,7 @@ namespace diskusjonsforum.Models
 {
 	public class User
 	{
-		public int UserID { get; set; }
+		public int UserID { get; set; } 
         public String Name { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
         public String Email { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
         public String PasswordHash { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
