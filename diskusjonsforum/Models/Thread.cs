b01a028df6c9df288eb3fa1ced9541d15ecf54ab
szstuff <stilianzagorov1@gmail.com>
2023-09-20 01:14:25 +0200

    Corrected issues with DB Database will now be created automatically when building if not present Thread and User models, controllers and listviews are now correctly implemented such that the contents of tables User and Thread can be accessed

--- diskusjonsforum/Models/Thread.cs
+++ diskusjonsforum/Models/Thread.cs
@@ -1,14 +1,19 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using SQLite;
+using SQLitePCL;
+
 namespace diskusjonsforum.Models
 {
     public class Thread
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Title { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
         public string Category { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
         public string? Description { get; set; }
         public DateTime CreatedAt { get; set; }
-        public User CreatedBy { get; set; } = null!; //null! "promises" the compiler that the value wont be null. Use Regex!
+        [ForeignKey("User")]
+        public int CreatedBy { get; set; }
         public ICollection<Comment>? Posts { get; set; }
     }
 }
