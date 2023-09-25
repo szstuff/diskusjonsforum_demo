760d705750ffcab429722c20682ec0e6eb1798c2
Jenny Hovland <jenny.hovland@hva.nl>
2023-09-25 14:14:20 +0200

    Added css code for home index, .custom-font

--- diskusjonsforum/DAL/ThreadDbContext.cs
+++ diskusjonsforum/DAL/ThreadDbContext.cs
@@ -1,17 +1,13 @@
 using System;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace diskusjonsforum.Models;
 
-public class ThreadDbContext : IdentityDbContext
-//public class ThreadDbContext : DbContext  // Changed to IdentityDbContext as described in canvas -> Demo:Authentication and authorisation
-
+public class ThreadDbContext : DbContext
 {
 	public ThreadDbContext(DbContextOptions<ThreadDbContext> options) : base(options)
 	{
-		//Database.EnsureDeleted(); //Deletes database on each run to ensure it's "refreshed" every time 
-		Database.EnsureCreated();
+		//Database.EnsureCreated();
 	}
 
     public DbSet<Thread> Threads { get; set; }
