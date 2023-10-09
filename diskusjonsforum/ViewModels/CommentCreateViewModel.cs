464402e18db3bb052691c684c764a4903a3011ff
stilian <stilianzagorov1@gmail.com>
2023-10-09 11:37:29 +0200

    Comments can now be edited. Comments and threads show their creation date and, if edited, the time they were last edited

--- diskusjonsforum/ViewModels/CommentCreateViewModel.cs
+++ diskusjonsforum/ViewModels/CommentCreateViewModel.cs
@@ -8,6 +8,7 @@ public class CommentCreateViewModel
     public int ThreadId { get; set; }
     public int ParentCommentId { get; set; }
     public Comment? ParentComment { get; set; }
+    public Comment? CommentToEdit { get; set; }
     public Comment? Comment { get; set; }
     public Thread Thread { get; set; }
 }
