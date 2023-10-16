70c10548d2b4c07792bf7294bc630faf2ecab1a5
Saloni Arora Chitkara <s364538@oslomet.no>
2023-10-16 14:31:44 +0200

    Authentication: Sletta DAL, lasta ned ny NuGet packages og endret dem til riktig version

--- diskusjonsforum/Program.cs
+++ diskusjonsforum/Program.cs
@@ -1,7 +1,10 @@
 using Diskusjonsforum.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using diskusjonsforum.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("diskusjonsforumIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'diskusjonsforumIdentityDbContextConnection' not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -10,6 +13,13 @@ builder.Services.AddDbContext<ThreadDbContext>(options => {
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:ThreadDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<diskusjonsforumIdentityDbContext>();
+
+
+/*builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ThreadDbContext>();*/
 
 var app = builder.Build();
 
@@ -35,5 +45,7 @@ app.UseEndpoints(endpoints =>
 });
 
 app.MapDefaultControllerRoute();
+
+app.UseAuthentication();
 
 app.Run();
