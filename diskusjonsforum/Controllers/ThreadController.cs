2030fa7d388ae362b742808bf0c8d59a79d5de8b
Joviank <Joviak@hotmail.no>
2023-10-17 16:34:28 +0200

    Rydder opp og sletter mapper som ikke trengs lenger ettersom at authentication er på plass

--- diskusjonsforum/Controllers/ThreadController.cs
+++ diskusjonsforum/Controllers/ThreadController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Diskusjonsforum.Models;
 using diskusjonsforum.ViewModels;
