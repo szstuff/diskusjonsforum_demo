a0555ed7ba25caa9e08f2cec379288e10fe32c9d
linnvu <linn20011@gmail.com>
2023-10-26 19:34:44 +0200

    comments

--- diskusjonsforum/Models/Category.cs
+++ diskusjonsforum/Models/Category.cs
@@ -1,3 +1,5 @@
+
+/*class for category*/
 using System.Collections;
 
 namespace Diskusjonsforum.Models;
@@ -5,8 +7,8 @@ namespace Diskusjonsforum.Models;
 public class Category : IEnumerable
 {
     public string CategoryName { get; set; }
-    public string? CategoryDescription { get; set; }
-    public bool RestrictedToAdmins { get; set; } = false; 
+    public string? CategoryDescription { get; set; } /*the questionmark indicates that it can be nullable*/
+    public bool RestrictedToAdmins { get; set; } = false;  /*set to false meaning the category is not restricted to admins by default*/
     public List<Thread>? ThreadsInCategory { get; set; }
 
 
