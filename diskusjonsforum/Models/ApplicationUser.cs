c8c983c95e8d579ff514a2449d7b11e9bd594f7a
stilian <48431339+szstuff@users.noreply.github.com>
2023-10-30 20:41:29 +0100

    Update ApplicationUser.cs comments

--- diskusjonsforum/Models/ApplicationUser.cs
+++ diskusjonsforum/Models/ApplicationUser.cs
@@ -1,9 +1,10 @@
-﻿
+
 using Microsoft.AspNetCore.Identity;
 
 namespace Diskusjonsforum.Models;
 
 public class ApplicationUser : IdentityUser
 {
-
+    //ApplicationUser was implemented in case we want to add properties to the Used in the future 
+    //The model is currently just a regular IdentityUser
 }
