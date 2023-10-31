34a88c646e1a99236cd052291129ae385d7497d2
szstuff <stilianzagorov1@gmail.com>
2023-10-31 04:48:15 +0100

    Pusset på utseende av Thread view og Comment Create view Fjernet ubrukte properties i metoder som Category og oppdaterte databasen Opprettet mye dummy data for eksamensoppgaven

--- diskusjonsforum/Models/Category.cs
+++ diskusjonsforum/Models/Category.cs
@@ -6,8 +6,7 @@ namespace Diskusjonsforum.Models;
 
 public class Category
 {
-    public string CategoryName { get; set; }
-    public string? CategoryDescription { get; set; } // Some categories have descriptions, but we decided that the name is self-explanatory. Description is nullable as it's not used or required 
-    public bool RestrictedToAdmins { get; set; } = false;  //Same as above: Unused property of Category. Defaults to false 
+    public string CategoryName { get; set; } //Doubles as PK 
+
     public List<Thread>? ThreadsInCategory { get; set; }
 }
